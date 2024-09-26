646e8ec4b6f4dea9a3337774d1e5eebebbc1a573
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-09-26 23:01:27 +0200

    Made adjustments to match scrimba code-along

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+
+//ReactDOM.render(<App />, document.getElementById("root"))
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
